bc52dd45dbd12140ed706c5a0b5c0071d65300f9
Cláudio Diniz <cdiniz@equalexperts.com>
2016-10-04 20:35:34 +0100

    Add beginning of unit testing for user dao

---
+++ test/models/daos/UserDAOSpec.scala
@@ -0,0 +1,61 @@
+package models.daos
+
+import java.sql.Timestamp
+import com.google.inject.AbstractModule
+import com.mohiva.play.silhouette.api.LoginInfo
+import com.typesafe.config.ConfigFactory
+import models.persistence.User
+import net.codingwell.scalaguice.ScalaModule
+import org.specs2.mock.Mockito
+import org.specs2.specification.Scope
+import play.api.Configuration
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.{ PlaySpecification, WithApplication }
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+/**
+ * Test case for the [[models.daos.UserDAO]] class.
+ */
+class UserDAOSpec extends PlaySpecification with Mockito {
+  sequential
+  "user dao" should {
+    "save and read a user" in new Context {
+      new WithApplication(application) {
+        val userDao = application.injector.instanceOf(classOf[UserDAO])
+        private val now: Timestamp = new Timestamp(System.currentTimeMillis())
+        Await.result(
+          userDao.save(User(0, LoginInfo("", ""), Some("name"), None, None, None, None, true, true, now, now)), 1 minute)
+        val u: Option[User] = Await.result(
+          userDao.find(1), 1 minute)
+
+        u.get.id shouldEqual 1
+        u.get.firstName shouldEqual Some("name")
+      }
+    }
+  }
+
+  /**
+   * The context.
+   */
+  trait Context extends Scope {
+
+    /**
+     * A fake Guice module.
+     */
+    class FakeModule extends AbstractModule with ScalaModule {
+      def configure() = {
+        bind[UserDAO].to[UserDAOImpl]
+      }
+    }
+
+    /**
+     * The application.
+     */
+    lazy val application = new GuiceApplicationBuilder()
+      .overrides(new FakeModule)
+      .loadConfig(Configuration(ConfigFactory.load("application.test.conf")))
+      .build()
+  }
+
+}
--- test/models/services/UserServiceTest.scala
+++ test/models/services/UserServiceTest.scala
@@ -14,22 +14,21 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import play.api.test.PlaySpecification
 import org.specs2.mock.Mockito
 
-import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.{ Await, ExecutionContext, Future }
 /**
-  * Created by cdiniz on 03/10/16.
-  */
-class UserServiceTest(implicit ec: ExecutionContext) extends PlaySpecification with Mockito{
+ * Created by cdiniz on 03/10/16.
+ */
+class UserServiceTest(implicit ec: ExecutionContext) extends PlaySpecification with Mockito {
 
-
-  val sampleUser = User(1,LoginInfo("",""),None,None,None,None,None,true,true,new Timestamp(Clock().now.getMillis),new Timestamp(Clock().now.getMillis))
+  val sampleUser = User(1, LoginInfo("", ""), None, None, None, None, None, true, true, new Timestamp(Clock().now.getMillis), new Timestamp(Clock().now.getMillis))
 
   "UserService" should {
 
-    "retrieve user by id" in  {
+    "retrieve user by id" in {
       val daoMock = mock[UserDAO]
       val clock = mock[Clock]
-      val service = new UserServiceImpl(daoMock,clock)
-      val returnUser = Future.successful{ Some(sampleUser)}
+      val service = new UserServiceImpl(daoMock, clock)
+      val returnUser = Future.successful { Some(sampleUser) }
       daoMock.find(1) returns returnUser
 
       service.retrieve(1) shouldEqual returnUser
@@ -37,12 +36,12 @@ class UserServiceTest(implicit ec: ExecutionContext) extends PlaySpecification w
       there was one(daoMock).find(1)
     }
 
-    "retrieve user by loginInfo" in  {
+    "retrieve user by loginInfo" in {
       val daoMock = mock[UserDAO]
       val clock = mock[Clock]
-      val service = new UserServiceImpl(daoMock,clock)
-      val returnUser = Future.successful{ Some(sampleUser)}
-      val loginInfo = LoginInfo("id","key")
+      val service = new UserServiceImpl(daoMock, clock)
+      val returnUser = Future.successful { Some(sampleUser) }
+      val loginInfo = LoginInfo("id", "key")
       daoMock.find(loginInfo) returns returnUser
 
       service.retrieve(loginInfo) shouldEqual returnUser
@@ -50,12 +49,12 @@ class UserServiceTest(implicit ec: ExecutionContext) extends PlaySpecification w
       there was one(daoMock).find(loginInfo)
     }
 
-    "save user" in  {
+    "save user" in {
       val daoMock = mock[UserDAO]
       val clock = mock[Clock]
-      val service = new UserServiceImpl(daoMock,clock)
-      val returnUser = Future.successful{sampleUser}
-      val loginInfo = LoginInfo("id","key")
+      val service = new UserServiceImpl(daoMock, clock)
+      val returnUser = Future.successful { sampleUser }
+      val loginInfo = LoginInfo("id", "key")
       daoMock.save(sampleUser) returns returnUser
 
       service.save(sampleUser) shouldEqual returnUser
@@ -66,16 +65,16 @@ class UserServiceTest(implicit ec: ExecutionContext) extends PlaySpecification w
     "save an existing user with commonSocialProfile" in {
       val daoMock = mock[UserDAO]
       val clock = mock[Clock]
-      val service = new UserServiceImpl(daoMock,clock)
-      val returnUser = Future.successful{Some(sampleUser)}
-      val loginInfo = LoginInfo("id","key")
-      val commonsProfile = CommonSocialProfile(loginInfo,Some("first"),Some("last"),Some("full"),Some("email"),Some("avatar"))
-      val userWithProfileInfo = sampleUser.copy( firstName = Some("first"), lastName = Some("last"), fullName = Some("full"), email = Some("email"), avatarURL = Some("avatar"))
-      val returnUserWithProfileInfo = Future{userWithProfileInfo}
+      val service = new UserServiceImpl(daoMock, clock)
+      val returnUser = Future.successful { Some(sampleUser) }
+      val loginInfo = LoginInfo("id", "key")
+      val commonsProfile = CommonSocialProfile(loginInfo, Some("first"), Some("last"), Some("full"), Some("email"), Some("avatar"))
+      val userWithProfileInfo = sampleUser.copy(firstName = Some("first"), lastName = Some("last"), fullName = Some("full"), email = Some("email"), avatarURL = Some("avatar"))
+      val returnUserWithProfileInfo = Future { userWithProfileInfo }
       daoMock.find(loginInfo) returns returnUser
       daoMock.save(userWithProfileInfo) returns returnUserWithProfileInfo
 
-      Await.result(service.save(commonsProfile),1 minute) shouldEqual userWithProfileInfo
+      Await.result(service.save(commonsProfile), 1 minute) shouldEqual userWithProfileInfo
 
       there was one(daoMock).find(loginInfo)
       there was one(daoMock).save(userWithProfileInfo)
@@ -85,20 +84,20 @@ class UserServiceTest(implicit ec: ExecutionContext) extends PlaySpecification w
     "save a new user with commonSocialProfile" in {
       val daoMock = mock[UserDAO]
       val clock = mock[Clock]
-      val service = new UserServiceImpl(daoMock,clock)
-      val returnNone = Future.successful{None}
-      val loginInfo = LoginInfo("id","key")
+      val service = new UserServiceImpl(daoMock, clock)
+      val returnNone = Future.successful { None }
+      val loginInfo = LoginInfo("id", "key")
       val userCreationTime = DateTime.now
       val userCreationInstant = new Timestamp(DateTime.now.getMillis)
-      val commonsProfile = CommonSocialProfile(loginInfo,Some("first"),Some("last"),Some("full"),Some("email"),Some("avatar"))
+      val commonsProfile = CommonSocialProfile(loginInfo, Some("first"), Some("last"), Some("full"), Some("email"), Some("avatar"))
       val userWithProfileInfo = User(id = 0, loginInfo = loginInfo, firstName = Some("first"), lastName = Some("last"), fullName = Some("full"), email = Some("email"), avatarURL = Some("avatar"), activated = true, explicitContentAuth = false, userCreationInstant, userCreationInstant)
-      val returnUserWithProfileInfo = Future{userWithProfileInfo}
+      val returnUserWithProfileInfo = Future { userWithProfileInfo }
 
       clock.now returns userCreationTime
       daoMock.find(loginInfo) returns returnNone
       daoMock.save(userWithProfileInfo) returns returnUserWithProfileInfo
 
-      Await.result(service.save(commonsProfile),1 minute) shouldEqual userWithProfileInfo
+      Await.result(service.save(commonsProfile), 1 minute) shouldEqual userWithProfileInfo
 
       there was one(clock).now
       there was one(daoMock).find(loginInfo)
